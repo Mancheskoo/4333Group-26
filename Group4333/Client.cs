54f85feb74f401b3671bed51eafd75d5fe36fdc8
Artem <monic3936@gmail.com>
2026-02-16 23:30:00 +0300

    Лаба 2

---
+++ Group4333/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Clients
+{
+    public int ID { get; set; }
+    public string ClientCode { get; set; }
+    public string FullName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string IndexCode { get; set; }
+    public string City { get; set; }
+    public string Street { get; set; }
+    public string House { get; set; }
+    public string Apartment { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+}
---
+++ Group4333/MonichContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.Entity; // Если используешь .NET Framework
+
+namespace Group4333
+{
+    // Класс контекста — это "пульт управления" твоей базой данных
+    public class MonichContext : DbContext
+    {
+        // Конструктор: "MonichConnection" — это имя строки из App.config (создадим на след. шаге)
+        public MonichContext() : base("name=MonichConnection")
+        {
+        }
+
+        public DbSet<Clients> Clients { get; set; }
+    }
+
+    // Класс сущности (таблица Clients из твоей БД)
+    public class Clients
+    {
+        public int ID { get; set; }
+        public string ClientCode { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string IndexCode { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
